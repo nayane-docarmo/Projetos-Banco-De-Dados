81b06fdd6643da09502248c909c9dfee1b2af93b
Nayane Cristina <nayane.docarmo02@gmail.com>
2020-08-14 16:45:11 -0300

    Pets Concluido

---
+++ pets/SQLQuery4.sql
@@ -0,0 +1,46 @@
+/*Criando o banco pets*/
+CREATE DATABASE pets;
+
+/*Criando banco efetivamente*/
+USE pets;
+
+/*Criando tabelas*/
+CREATE TABLE clinica(
+	IdClinica INT IDENTITY PRIMARY KEY NOT NULL,
+	Endereço VARCHAR(100),
+);
+
+CREATE TABLE veterinario(
+	IdVeterinario INT IDENTITY PRIMARY KEY NOT NULL,
+	IdClinica INT FOREIGN KEY REFERENCES clinica (IdClinica),
+	Nome VARCHAR(50),
+	);
+
+CREATE TABLE animais(
+	IdAnimais INT IDENTITY PRIMARY KEY NOT NULL,
+	IdVeterinario INT FOREIGN KEY REFERENCES veterinario (IdVeterinario),
+	Nome VARCHAR(50),
+	DataNascimento DATE,
+);
+
+CREATE TABLE dono(
+	IdDono INT IDENTITY PRIMARY KEY NOT NULL,
+	IdAnimais INT FOREIGN KEY REFERENCES animais (IdAnimais)
+);
+
+CREATE TABLE raça(
+	IdRaça INT IDENTITY PRIMARY KEY NOT NULL,
+	IdAnimais INT FOREIGN KEY REFERENCES animais (IdAnimais),
+	Tipo VARCHAR(50),
+);
+
+CREATE TABLE atendimento(
+	IdAtentimento INT IDENTITY PRIMARY KEY NOT NULL,
+	IdVeterinario INT FOREIGN KEY REFERENCES veterinario (IdVeterinario),
+	IdAnimais INT FOREIGN KEY REFERENCES animais (IdAnimais),
+);
+
+
+
+
+
